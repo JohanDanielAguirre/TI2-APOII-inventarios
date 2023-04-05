007169b315ae0e82b26bb4350f1d84dd9e2e93f5
Samub96 <samuel.d.barona@gmail.com>
2023-04-05 11:36:05 -0500

    Estructura inicial de la TI2

--- MercadoLibre/src/model/Producto.java
+++ MercadoLibre/src/model/Producto.java
@@ -9,13 +9,32 @@ public class Producto {
         private Categoria categoria;
         private int vecesComprado;
 
-    public Producto(String nombre, String descripcion, double precio, int cantidadDisponible, Categoria categoria, int vecesComprado) {
+    public Producto(String nombre, String descripcion, double precio, int cantidadDisponible, int categorias) {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
         this.cantidadDisponible = cantidadDisponible;
-        this.categoria = categoria;
-        this.vecesComprado = vecesComprado;
+
+        switch (categorias){
+
+            case 1: categoria = Categoria.LIBROS;
+                break;
+            case 2: categoria = Categoria.ELECTRONICA;
+                break;
+            case 3: categoria = Categoria.ROPA_Y_ACCESORIOS;
+                break;
+            case 4: categoria = Categoria.ALIMENTOS_Y_BEBIDAS;
+                break;
+            case 5: categoria = Categoria.PAPELERIA;
+                break;
+            case 6: categoria = Categoria.DEPORTES;
+                break;
+            case 7: categoria = Categoria.BELLEZA_Y_CUIDADO_PERSONAL;
+                break;
+            case 8: categoria = Categoria.JUGUETES_Y_JUEGOS;
+                break;
+        }
+
     }
 
     public String getNombre() {
@@ -48,14 +67,6 @@ public class Producto {
 
     public void setCantidadDisponible(int cantidadDisponible) {
         this.cantidadDisponible = cantidadDisponible;
-    }
-
-    public Categoria getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
     }
 
     public int getVecesComprado() {

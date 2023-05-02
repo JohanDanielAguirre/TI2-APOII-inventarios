32e2b5ebe75f33e550d61ba7f87286b3d4ab9a6c
SebasL07 <100957510+SebasL07@users.noreply.github.com>
2023-05-02 12:23:48 -0500

    classes user and seller erased, started controller

--- MercadoLibre/src/model/Controller.java
+++ MercadoLibre/src/model/Controller.java
@@ -1,4 +1,14 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Controller {
+
+    public ArrayList<Product> products;
+    public ArrayList<Delivery> deliveries;
+
+    public void addProducts(String nombre, String descripcion, double precio, int cantidadDisponible, int categorias){
+        products.add(new Product(nombre,descripcion,precio,cantidadDisponible,categorias));
+    }
+    
 }
--- MercadoLibre/src/model/Seller.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-public class Seller extends User {
-
-
-    public Seller(String username, String password, String name) {
-        super(username, password, name);
-    }
-}

478bb0c309172dd2b7c14042b0645b2123d93985
Samub96 <samuel.d.barona@gmai.com>
2023-05-04 14:01:03 -0500

    Binary Search

--- MercadoLibre/src/model/Controller.java
+++ MercadoLibre/src/model/Controller.java
@@ -12,4 +12,22 @@ public class Controller {
         Comprador comprador = new Comprador(username, password, name);
         return false;
     }
+    public  int binarySearch(int[] arr, int key) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] == key) {
+                return mid;
+            } else if (arr[mid] < key) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
 }

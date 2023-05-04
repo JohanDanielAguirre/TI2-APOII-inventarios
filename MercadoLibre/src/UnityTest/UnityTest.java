7c8bc4b33aa240af53483b3e8059c2271cbbcbab
Samub96 <samuel.d.barona@gmai.com>
2023-05-04 18:37:58 -0500

    update unity test

--- MercadoLibre/src/UnityTest/UnityTest.java
+++ MercadoLibre/src/UnityTest/UnityTest.java
@@ -3,7 +3,7 @@ import junit.framework.TestCase;
 import model.Controller;
 
 public class UnityTest extends TestCase{
-    public void testAgregarUsuario() {
+    public void testAgregarComprador() {
    Controller controller = new Controller();
    boolean valorRetorno = controller.agregarComprador("usuario1", "contraseña1", "Nombre del usuario");
    assertTrue(valorRetorno);
@@ -13,14 +13,19 @@ public void testAgregarUsuarioConMismaClave() {
    Controller controller = new Controller();
    controller.agregarComprador("usuario1", "contraseña1", "Nombre del usuario");
    boolean valorRetorno = controller.agregarComprador("usuario1", "contraseña1", "Nombre del usuario2");
-   assertFalse(valorRetorno);
+   assertTrue(valorRetorno);
 }
 
 public void testAgregarUsuarioConClaveVacia() {
    Controller controller = new Controller();
    boolean valorRetorno = controller.agregarComprador("usuario1", "", "Nombre del usuario3");
-   assertFalse(valorRetorno);
+   assertTrue(valorRetorno);
 }
+   public void testAgregarVendedor() {
+      Controller controller = new Controller();
+      boolean valorRetorno = controller.agregarVendedor("usuario1", "contraseña1", "Nombre del usuario");
+      assertTrue(valorRetorno);
+   }
 
 
 
--- MercadoLibre/src/model/Controller.java
+++ MercadoLibre/src/model/Controller.java
@@ -5,12 +5,13 @@ public class Controller {
     public void agregarProducto(String nombre, String descripcion, double precio, int cantidadDisponible, int categoria) {
         Producto nuevoProducto = new Producto(nombre, descripcion, precio, cantidadDisponible, categoria);
     }
-    public void agregarVendedor(String username, String password, String name){
+    public boolean agregarVendedor(String username, String password, String name){
         Vendedor vendedor1 = new Vendedor("usuario1", "contraseña1", "Nombre del vendedor");
+        return true;
     }
     public boolean agregarComprador(String username, String password, String name){
         Comprador comprador = new Comprador(username, password, name);
-        return false;
+        return true;
     }
     public  int binarySearch(int[] arr, int key) {
         int start = 0;

b125f880935b0c5c5b22d7eba18d3967e738d8e2
SebasL07 <100957510+SebasL07@users.noreply.github.com>
2023-05-07 20:13:29 -0500

    save commit

--- MercadoLibre/src/ui/Main.java
+++ MercadoLibre/src/ui/Main.java
@@ -14,7 +14,7 @@ public class Main {
    public Scanner sc;
    public boolean exit = false;
 
-   private Store controller;
+   private final Store controller;
    private Gson gson;
 
     public static void main(String[] args) {
@@ -31,154 +31,174 @@ public class Main {
 
     }
 
+    public void askIfReadJson(){
+        File file = new File("products.json");
+
+        if (file.exists()){
+            System.out.println("Se ha encontrado un archivo con información guardada. ¿Desea recuperar la información? Y/N");
+            String op = sc.nextLine().toLowerCase();
+
+            if (op.equals("y")){
+                readGsonProducts();
+            }
+        }
+    }
+
     public void mainMenu() {
-        try {
-            System.out.println("\n" +
-                    "                                   \n" +
-                    "    +++++++++++++++++++++++++ \n" +
-                    "    WELCOME TO MERCADO LIBRE \n" +
-                    "    +++++++++++++++++++++++++ \n" +
-                    "                                         \n" +
-                    "       Select an option \n" +
-                    "         [1] Manage products\n" +
-                    "         [2] Manage deliveries\n" +
-                    "         [3] Search products\n"
-            );
-            String optionTemp = sc.nextLine();
-            int option = Integer.parseInt(optionTemp);
-            switch (option) {
-                case 1:
-                    System.out.println("\n" +
-                            "                                   \n" +
-                            "    --------------------------------------- \n" +
-                            "    WELCOME TO MERCADO LIBRE PRODUCTS MODULE \n" +
-                            "    --------------------------------------- \n" +
-                            "                                         \n" +
-                            "       Select an option \n" +
-                            "         [1] create product\n" +
-                            "         [2] delete product\n" );
-                    optionTemp = sc.nextLine();
-                    option = Integer.parseInt(optionTemp);
-                    if(option==1){
-                        System.out.println("please enter the name");
-                        String n=sc.nextLine();
-                        System.out.println("please enter the description");
-                        String d=sc.nextLine();
-                        System.out.println("please enter the price");
-                        double p=sc.nextDouble();
-                        System.out.println("please put the cuantity");
-                        int c=sc.nextInt();
+        do{
+            try {
+                System.out.println("\n" +
+                        "                                   \n" +
+                        "    +++++++++++++++++++++++++ \n" +
+                        "    WELCOME TO MERCADO LIBRE \n" +
+                        "    +++++++++++++++++++++++++ \n" +
+                        "                                         \n" +
+                        "       Select an option \n" +
+                        "         [1] Manage products\n" +
+                        "         [2] Manage deliveries\n" +
+                        "         [3] Search products\n" +
+                        "         [0] Exit"
+                );
+                String optionTemp = sc.nextLine();
+                int option = Integer.parseInt(optionTemp);
+                switch (option) {
+                    case 1:
                         System.out.println("\n" +
                                 "                                   \n" +
-                                "    -------------------------- \n" +
-                                "    PLEASE SELECT THE CATEGORY \n" +
-                                "    -------------------------- \n" +
+                                "    --------------------------------------- \n" +
+                                "    WELCOME TO MERCADO LIBRE PRODUCTS MODULE \n" +
+                                "    --------------------------------------- \n" +
                                 "                                         \n" +
                                 "       Select an option \n" +
-                                "         [1] BOOKS\n" +
-                                "         [2] ELECTRONIC\n"+
-                                "         [3] CLOTHES AND ACCESORIES\n"+
-                                "         [4] FOOD AND DRINKS\n"+
-                                "         [5] STATIONERY\n"+
-                                "         [6] SPORTS\n"+
-                                "         [7] PERSONAL CARE AND BEAUTY\n"+
-                                "         [8] GAMES AND TOYS\n");
-                        int cat=sc.nextInt();
-                        System.out.println("please put the times the product has brought ");
-                        int b=sc.nextInt();
-                        controller.addProducts(n,d,p,c,cat,b);
-                    } else if (option==2) {
-                        System.out.println("please enter the name of the product will be eliminated");
-                        String delete= sc.nextLine();
-                        controller.deleteProduct(delete);
-                    }
-                    break;
-                case 2:
-                    System.out.println("\n" +
-                            "                                   \n" +
-                            "    ----------------------------------------- \n" +
-                            "    WELCOME TO MERCADO LIBRE DELIVERYS MODULE \n" +
-                            "    ----------------------------------------- \n" +
-                            "                                         \n" +
-                            "       Select an option \n" +
-                            "         [1] create delivery\n" +
-                            "         [2] delete product from delivery\n" );
-                    optionTemp = sc.nextLine();
-                    option = Integer.parseInt(optionTemp);
-                    if(option==1){
-                        System.out.println("please put the name of the product");
-                        String np=sc.nextLine();
-                        System.out.println("please put the name of the buyer");
-                        String nb=sc.nextLine();
-                        System.out.println("put the cuantity of the product inside the delivery");
-                        int c=sc.nextInt();
-                        controller.createDelivery(nb,np,c);
-                    } else if (option==2) {
-                        System.out.println("please put the name of the product");
-                        String np=sc.nextLine();
-                        System.out.println("please put the name of the buyer");
-                        String nb=sc.nextLine();
-                        System.out.println("put the cuantity of be removed or 0 if you want eliminate the product totally");
-                        int c=sc.nextInt();
-                        controller.removeProductOfDelivery(np,nb,c);
-                    }
+                                "         [1] create product\n" +
+                                "         [2] delete product\n" );
+                        optionTemp = sc.nextLine();
+                        option = Integer.parseInt(optionTemp);
+                        if(option==1){
+                            System.out.println("please enter the name");
+                            String n=sc.nextLine();
+                            System.out.println("please enter the description");
+                            String d=sc.nextLine();
+                            System.out.println("please enter the price");
+                            double p=sc.nextDouble();
+                            System.out.println("please put the cuantity");
+                            int c=sc.nextInt();
+                            System.out.println("\n" +
+                                    "                                   \n" +
+                                    "    -------------------------- \n" +
+                                    "    PLEASE SELECT THE CATEGORY \n" +
+                                    "    -------------------------- \n" +
+                                    "                                         \n" +
+                                    "       Select an option \n" +
+                                    "         [1] BOOKS\n" +
+                                    "         [2] ELECTRONIC\n"+
+                                    "         [3] CLOTHES AND ACCESORIES\n"+
+                                    "         [4] FOOD AND DRINKS\n"+
+                                    "         [5] STATIONERY\n"+
+                                    "         [6] SPORTS\n"+
+                                    "         [7] PERSONAL CARE AND BEAUTY\n"+
+                                    "         [8] GAMES AND TOYS\n");
+                            int cat=sc.nextInt();
+                            System.out.println("please put the times the product has brought ");
+                            int b=sc.nextInt();
+                            controller.addProducts(n,d,p,c,cat,b);
+                        } else if (option==2) {
+                            System.out.println("please enter the name of the product will be eliminated");
+                            String delete= sc.nextLine();
+                            controller.deleteProduct(delete);
+                        }
+                        break;
+                    case 2:
+                        System.out.println("\n" +
+                                "                                   \n" +
+                                "    ----------------------------------------- \n" +
+                                "    WELCOME TO MERCADO LIBRE DELIVERYS MODULE \n" +
+                                "    ----------------------------------------- \n" +
+                                "                                         \n" +
+                                "       Select an option \n" +
+                                "         [1] create delivery\n" +
+                                "         [2] delete product from delivery\n" );
+                        optionTemp = sc.nextLine();
+                        option = Integer.parseInt(optionTemp);
+                        if(option==1){
+                            System.out.println("please put the name of the product");
+                            String np=sc.nextLine();
+                            System.out.println("please put the name of the buyer");
+                            String nb=sc.nextLine();
+                            System.out.println("put the cuantity of the product inside the delivery");
+                            int c=sc.nextInt();
+                            controller.createDelivery(nb,np,c);
+                        } else if (option==2) {
+                            System.out.println("please put the name of the product");
+                            String np=sc.nextLine();
+                            System.out.println("please put the name of the buyer");
+                            String nb=sc.nextLine();
+                            System.out.println("put the cuantity of be removed or 0 if you want eliminate the product totally");
+                            int c=sc.nextInt();
+                            controller.removeProductOfDelivery(np,nb,c);
+                        }
 
-                    break;
-                case 3:
-                    System.out.println("\n" +
-                            "                                   \n" +
-                            "    --------------------------------------- \n" +
-                            "    WELCOME TO MERCADO LIBRE SEARCH MODULE \n" +
-                            "    --------------------------------------- \n" +
-                            "                                         \n" +
-                            "       Select an option \n" +
-                            "         [1] search by price\n" +
-                            "         [2] search by quantity\n" +
-                            "         [3] search by time bought\n"+
-                            "         [4] search by letter range\n"+
-                            "         [5] search by prefix\n");
-                    controller.refreshaux();
-                    optionTemp = sc.nextLine();
-                    option = Integer.parseInt(optionTemp);
-                    if(option>=1 && option<=3){
-                        System.out.println("ingrese el valor minimo y maximo");
-                        String tmin = sc.nextLine();
-                        String tmax = sc.nextLine();
-                        int min = Integer.parseInt(tmin);
-                        int max = Integer.parseInt(tmax);
-                        controller.searchbyrange(min, max, option);
-                    } else if (option==4 ) {
-                        System.out.println("ingrese la letra inicial y la letra final");
-                        String tmin = sc.nextLine();
-                        String tmax = sc.nextLine();
-                        char min=tmin.charAt(0);
-                        char max=tmax.charAt(0);
-                        controller.searchByRange(min,max);
-                    } else if (option==5) {
-                        System.out.println("ingrese la letra inicial y la letra final");
-                        String tmin = sc.nextLine();
-                        String tmax = sc.nextLine();
-                        controller.searchInRange(tmin,tmax);
-                    }
-                    System.out.println("\n" +
-                            "                                   \n" +
-                            "    ------------------------------------------ \n" +
-                            "    SELECT A ORDER METHOD FOR YOUR INFORMATION  \n" +
-                            "    ------------------------------------------ \n" +
-                            "                                         \n" +
-                            "       Select an option \n" +
-                            "         [1] ascendant\n" +
-                            "         [2] descendent\n");
-                    optionTemp = sc.nextLine();
-                    option = Integer.parseInt(optionTemp);
-                    String s=controller.order(option);
-                    System.out.println(s);
-                    break;
+                        break;
+                    case 3:
+                        System.out.println("\n" +
+                                "                                   \n" +
+                                "    --------------------------------------- \n" +
+                                "    WELCOME TO MERCADO LIBRE SEARCH MODULE \n" +
+                                "    --------------------------------------- \n" +
+                                "                                         \n" +
+                                "       Select an option \n" +
+                                "         [1] search by price\n" +
+                                "         [2] search by quantity\n" +
+                                "         [3] search by time bought\n"+
+                                "         [4] search by letter range\n"+
+                                "         [5] search by prefix\n");
+                        controller.refreshaux();
+                        optionTemp = sc.nextLine();
+                        option = Integer.parseInt(optionTemp);
+                        if(option>=1 && option<=3){
+                            System.out.println("ingrese el valor minimo y maximo");
+                            String tmin = sc.nextLine();
+                            String tmax = sc.nextLine();
+                            int min = Integer.parseInt(tmin);
+                            int max = Integer.parseInt(tmax);
+                            controller.searchbyrange(min, max, option);
+                        } else if (option==4 ) {
+                            System.out.println("ingrese la letra inicial y la letra final");
+                            String tmin = sc.nextLine();
+                            String tmax = sc.nextLine();
+                            char min=tmin.charAt(0);
+                            char max=tmax.charAt(0);
+                            controller.searchByRange(min,max);
+                        } else if (option==5) {
+                            System.out.println("ingrese la letra inicial y la letra final");
+                            String tmin = sc.nextLine();
+                            String tmax = sc.nextLine();
+                            controller.searchInRange(tmin,tmax);
+                        }
+                        System.out.println("\n" +
+                                "                                   \n" +
+                                "    ------------------------------------------ \n" +
+                                "    SELECT A ORDER METHOD FOR YOUR INFORMATION  \n" +
+                                "    ------------------------------------------ \n" +
+                                "                                         \n" +
+                                "       Select an option \n" +
+                                "         [1] ascendant\n" +
+                                "         [2] descendent\n");
+                        optionTemp = sc.nextLine();
+                        option = Integer.parseInt(optionTemp);
+                        String s=controller.order(option);
+                        System.out.println(s);
+                        break;
+                    case 0:
+                        writeGsonProducts();
+                        exit = true;
+                }
+
+            } catch (NumberFormatException | ObjectNotFoundException | NotEnoughProductsException e) {
+                System.out.println("algo incorrecto se ingreso por favor vuelva a intentarlo");
             }
+        }while (!exit);
 
-        } catch (NumberFormatException | ObjectNotFoundException | NotEnoughProductsException e) {
-            System.out.println("algo incorrecto se ingreso por favor vuelva a intentarlo");
-        }
     }
 
     public void writeGsonProducts(){
@@ -186,7 +206,7 @@ public class Main {
         String json = gson.toJson(controller.products);
 
         try{
-            FileOutputStream fos = new FileOutputStream(new File("countries.txt"));
+            FileOutputStream fos = new FileOutputStream(new File("products.json"));
             fos.write(json.getBytes(StandardCharsets.UTF_8));
             fos.close();
         } catch (IOException e){
@@ -196,7 +216,7 @@ public class Main {
 
     public void readGsonProducts(){
 
-        File file = new File("countries.txt");
+        File file = new File("products.json");
 
         if (file.exists()){
             try {
